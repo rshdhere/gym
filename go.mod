01df4d6995b1e17934fdb6ab11cab2ab2a817322
raashed <mohammedraashed10@gmail.com>
2025-12-05 15:20:38 +0530

    feat: introduced chi-router

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/rshdhere/gym
 
 go 1.23.4
+
+require github.com/go-chi/chi/v5 v5.2.3
---
+++ internal/routes/routes.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/rshdhere/gym/internal/app"
+)
+
+func SetupRoutes(app *app.Application) *chi.Mux {
+	r := chi.NewRouter()
+
+	r.Get("/health", app.HealthCheck)
+	return r
+}
