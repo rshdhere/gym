527dc5c635c3e9edb62e85dc27ce79b5346a12be
raashed <mohammedraashed10@gmail.com>
2025-12-05 15:40:23 +0530

    feat(api): implement workout handler with CRUD operations - Add WorkoutHandler struct with methods for handling workout requests. - Implement HandleGetWorkoutById method for retrieving workout by ID. - Implement HandleCreateWorkout method for creating a new workout. - Update application and routing to include workout handler endpoints.

--- internal/app/app.go
+++ internal/app/app.go
@@ -5,17 +5,27 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/rshdhere/gym/internal/api"
 )
 
 type Application struct {
-	Logger *log.Logger
+	Logger         *log.Logger
+	WorkoutHandler *api.WorkoutHandler
 }
 
 func NewApplication() (*Application, error) {
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
+	// our store will go in here
+
+	// our handlers will go in here
+
+	workoutHandler := api.NewWorkoutHandler()
+
 	app := &Application{
-		Logger: logger,
+		Logger:         logger,
+		WorkoutHandler: workoutHandler,
 	}
 
 	return app, nil
--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -9,5 +9,8 @@ func SetupRoutes(app *app.Application) *chi.Mux {
 	r := chi.NewRouter()
 
 	r.Get("/health", app.HealthCheck)
+	r.Get("/workouts/{id}", app.WorkoutHandler.HandleGetWorkoutById)
+
+	r.Post("/workouts", app.WorkoutHandler.HandleCreateWorkout)
 	return r
 }
